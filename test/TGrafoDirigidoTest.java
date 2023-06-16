6dafa78543a24d5a87d711168d120181e7944fb0
IgnacioPerez98 <85960357+IgnacioPerez98@users.noreply.github.com>
2023-06-16 20:52:14 -0300

    changes

--- test/TGrafoDirigidoTest.java
+++ test/TGrafoDirigidoTest.java
@@ -61,25 +61,42 @@ public class TGrafoDirigidoTest {
 
     @Test
     public void testExisteAristaTrue() {
-        TArista arista = new TArista("2","1",100);
+        TArista arista = new TArista("2", "1", 100);
         GrafoSimplif.insertarArista(arista);
         boolean actual = GrafoSimplif.existeArista("2", "1");
         assertTrue(actual);
-        
+
     }
+
     @Test
     public void testExisteAristaFalse() {
-        TArista arista = new TArista("2","1",100);
+        TArista arista = new TArista("2", "1", 100);
         boolean actual = GrafoSimplif.existeArista("2000", "1");
         assertFalse(actual);
     }
 
     @Test
     public void testExisteVertice() {
+        boolean expected = GrafoSimplif.existeVertice("3");
+        assertTrue(expected);
+    }
+
+    @Test
+    public void testExisteVerticeCuandoNoExiste() {
+        boolean expected = GrafoSimplif.existeVertice("10");
+        assertFalse(expected);
     }
 
     @Test
     public void testBuscarVertice() {
+        TVertice expected = GrafoSimplif.buscarVertice("3");
+        assertNotNull(expected);
+    }
+
+    @Test
+    public void testBuscarVerticeInexistente() {
+        TVertice expected = GrafoSimplif.buscarVertice("100");
+        assertNull(expected);
     }
 
     @Test
@@ -88,10 +105,19 @@ public class TGrafoDirigidoTest {
 
     @Test
     public void testInsertarVertice_Comparable() {
+        boolean expected = GrafoSimplif.existeVertice("4");
+        GrafoSimplif.insertarVertice("4");
+        boolean actual = GrafoSimplif.existeVertice("4");
+        assertEquals(actual, !expected);
     }
 
     @Test
     public void testInsertarVertice_TVertice() {
+        boolean expected = GrafoSimplif.existeVertice("4");
+        TVertice num4 = new TVertice("4");
+        GrafoSimplif.insertarVertice(num4);
+        boolean actual = GrafoSimplif.existeVertice("4");
+        assertEquals(actual, !expected);
     }
 
     @Test
@@ -100,6 +126,30 @@ public class TGrafoDirigidoTest {
 
     @Test
     public void testGetVertices() {
+        var vertices = GrafoSimplif.vertices.values();
+        //Creo los Vertices
+        LinkedList<TVertice> verticesprueba = new LinkedList<>();
+        TVertice vert1 = new TVertice("1");
+        TVertice vert2 = new TVertice("2");
+        TVertice vert3 = new TVertice("3");
+        verticesprueba.add(vert1);
+        verticesprueba.add(vert2);
+        verticesprueba.add(vert3);
+        
+        //Precondicion ambas colecciones tienen el mismo numero de elementos para hacer el test
+        boolean ctl = true;
+        var itExpexcted = verticesprueba.iterator();
+        var itGrafo = vertices.iterator();
+        while(itExpexcted.hasNext()){
+            Comparable etexpected = itExpexcted.next().getEtiqueta();
+            Comparable etgrafo = itGrafo.next().getEtiqueta();
+            if(etexpected.compareTo(etgrafo)!= 0){
+                ctl =false;
+                break;
+            }
+        }
+        assertTrue(ctl);
+        
     }
 
     @Test
